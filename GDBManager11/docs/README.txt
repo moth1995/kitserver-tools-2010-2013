98541cdb348d812980e42f1ba09757b747023e04
juce <juce66@gmail.com>
2011-02-24 10:02:12 -0800

    adding GDB Manager 11

---
+++ GDBManager11/docs/README.txt
@@ -0,0 +1,41 @@
+GDB Manager                                               February 24, 2011
+===========================================================================
+Version 11.0.0
+
+
+1. INTRODUCTION
+---------------
+
+GDB Manager 11 is a GUI front-end for KitServer 11 GDB (Game DataBase). 
+It simplifies the task of defining kit attributes, such as 3D-model of a 
+shirt, collar type, name location, name shape, and some others.
+GDB Manager will modify (and create, if necessary) the corresponding 
+"config.txt" files.
+
+
+2. USAGE
+--------
+
+Run GDBManager.exe. When run first time, it will ask you to select the
+location of your "GDB" folder. After that it should show a directory-like
+tree on the left side which enumerates all the kits in the GDB. 
+(IMPORTANT: Only kits that are mapped in your map.txt file will be shown)
+You can select a kit there and set/modify attributes in the right panel.
+
+2.1. NEW TOOL: color picker
+---------------------------
+Right click on the kit image and the color will be picked from the
+shirt and set as the kit main (radar) color. If you do a SHIFT-RightClick,
+then the picked color will be set as shorts color.
+
+
+
+3. CREDITS
+----------
+
+Programming: juce
+Third party tools and libraries: wxPython by Robin Dunn and Co.
+
+===========================================================================
+
+
---
+++ GDBManager11/setup.py
@@ -0,0 +1,75 @@
+# setup.py
+from distutils.core import setup
+import py2exe
+
+
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        # for the versioninfo resources
+        self.version = "9.0.2"
+        self.company_name = "Kitserver Ltd."
+        self.copyright = "Copyright 2010 Juce"
+        self.name = "GDB Manager"
+
+
+################################################################
+# A program using wxPython
+
+# The manifest will be inserted as resource into test_wx.exe.  This
+# gives the controls the Windows XP appearance (if run on XP ;-)
+#
+# Another option would be to store it in a file named
+# test_wx.exe.manifest, and copy it with the data_files option into
+# the dist-dir.
+#
+manifest_template = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(prog)s"
+    type="win32"
+/>
+<description>%(prog)s Program</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+'''
+
+RT_MANIFEST = 24
+
+gdbManager = Target(
+    # used for the versioninfo resource
+    description = "GDB Manager utility for Kitserver 2010 GDB",
+
+    # what to build
+    script = "GDBManager.py",
+    other_resources = [(RT_MANIFEST, 1, 
+            manifest_template % dict(prog="GDBManager"))],
+    #icon_resources = [(1, "icon.ico")],
+    ##dest_base = "gdbManager"
+)
+
+setup(
+    windows = [gdbManager],
+        data_files=[
+            (".",[
+                "gdbm.yaml",
+                "default.png",
+                "msvcp71.dll"]),
+            ("docs",[
+                "docs/README.txt"])
+        ]
+)

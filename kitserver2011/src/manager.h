ccf24f789236258c753db1ee79c7613107029121
juce <juce66@gmail.com>
2011-06-08 23:45:01 -0700

    window title tweak for manager

--- kitserver2011/src/manager.h
+++ kitserver2011/src/manager.h
@@ -2,9 +2,9 @@
 #define _MANAGER_MAIN_
 
 #ifdef MYDLL_RELEASE_BUILD
-#define SETUP_WINDOW_TITLE L"KitServer 2011 EXE Manager"
+#define SETUP_WINDOW_TITLE L"KitServer 11 EXE Manager"
 #else
-#define SETUP_WINDOW_TITLE L"KitServer 2011 EXE Manager (debug build)"
+#define SETUP_WINDOW_TITLE L"KitServer 11 EXE Manager (debug build)"
 #endif
 #define CREDITS L"About: v11.1.0 (05/2011) by Juce and Robbie."
 

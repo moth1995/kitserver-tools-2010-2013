1ed32e422a5c712e0da419779c157c28d76f4cee
juce <juce66@gmail.com>
2011-11-23 13:07:02 -0800

    doc update; version bump

--- kitserver12/src/manager.h
+++ kitserver12/src/manager.h
@@ -6,7 +6,7 @@
 #else
 #define SETUP_WINDOW_TITLE L"Kitserver 12 EXE Manager (debug build)"
 #endif
-#define CREDITS L"About: v12.1.0 (11/2011) by Juce and Robbie."
+#define CREDITS L"About: v12.1.1 (11/2011) by Juce and Robbie."
 
 #endif
 
